ca5af1ebac2fa4ca2baab957362af0b51502980f
drdh <drdhii@163.com>
2021-04-09 16:25:47 +0800

    smaller hidden states; message normalization

--- src/modules/critics/maddpg.py
+++ src/modules/critics/maddpg.py
@@ -29,13 +29,18 @@ class MADDPGCritic(nn.Module):
                              actions.contiguous().view(-1, self.n_actions * self.n_agents)], dim=-1)
         if self.args.msg_pass:
             msg_inputs = inputs.reshape(-1, self.args.n_agents, inputs.shape[-1])
+            msg_inputs = F.normalize(msg_inputs, dim=-1)
+            inputs = F.normalize(inputs, dim=-1)
             msg_up = inputs.new(msg_inputs.shape[0], self.args.msg_dim).zero_()
 
             for i in reversed(range(self.args.n_agents)):
                 msg_up = self.msg_net(th.cat([msg_inputs[:, i], msg_up], dim=-1))
+                msg_up = F.normalize(msg_up, dim=-1)
             msg_down = [msg_up]
             for i in range(self.args.n_agents - 1):
-                msg_down.append(self.msg_net(th.cat([msg_inputs[:, i], msg_down[i]], dim=-1)))
+                m_down = self.msg_net(th.cat([msg_inputs[:, i], msg_down[i]], dim=-1))
+                m_down = F.normalize(m_down)
+                msg_down.append(m_down)
             msgs = th.stack(msg_down, dim=1).reshape(-1, self.args.msg_dim)
             inputs_n_msg = th.cat([inputs, msgs], dim=-1)
             x = F.relu(self.fc1(inputs_n_msg))
